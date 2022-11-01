e03607790b99e6ac91978b783f2750b32155addc
Dan Horea <danhorea2010@gmail.com>
2022-11-01 09:59:20 +0200

    master-detail list functionallity (#12)

--- components/ingredients.item.js
+++ components/ingredients.item.js
@@ -1,11 +1,40 @@
 import {View, Text, Pressable} from 'react-native';
 import {ListItem} from '@react-native-material/core';
+import {useCallback, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-native';
+
 
 export const IngredientItem = props => {
+  const [macroString, setMacroString] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    props.macros.forEach(element => {
+      const {carbs, fat, protein} = element;
+      setMacroString(
+        'Carbs: ' + carbs + 'g | Fat: ' + fat + 'g | Protein: ' + protein + 'g',
+      );
+    });
+    setMacroString(
+      currentMacroString => currentMacroString + '\nType: ' + props.type,
+    );
+  }, []);
+
+  const handlePress = () => {
+    console.log("Pressed" + macroString);
+        navigate("/ingredient/" + (props.index+1))
+
+
+  }
 
   return (
     <View>
-      <ListItem key={props.index} title={props.name} />
+      <ListItem
+        key={props.index}
+        title={props.name}
+        secondaryText={macroString}
+        onPress={handlePress}
+      />
     </View>
   );
 };
