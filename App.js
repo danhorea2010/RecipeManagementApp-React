f484a90470f372c8d01ef6ae068d193636f23ada
Dan Horea <danhorea2010@gmail.com>
2022-11-01 07:35:56 +0200

    placeholder home page (#10)

--- App.js
+++ App.js
@@ -6,98 +6,77 @@
  * @flow strict-local
  */
 
-import React from 'react';
+ import React from 'react';
 
-import type { Node } from 'react';
-
-import { store } from "./actions/store"
-import { Provider } from "react-redux"
-import RecipeList from './components/recipe.list';
-
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-export const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <View>
-      <Provider store={store}>
-      <Text> Test</Text>
-      <Text> Test</Text>
-      <Text> Test</Text>
-      <Text> Test</Text>
-      <Text> Test</Text>
-        <RecipeList></RecipeList>
-      </Provider>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import type {Node} from 'react';
+ 
+ import {store} from './actions/store';
+ import {Provider} from 'react-redux';
+ import RecipeList from './components/recipe.list';
+ import {Button, Stack, Switch} from '@react-native-material/core';
+ import { 
+   useDimensions, 
+   useDeviceOrientation 
+ } from '@react-native-community/hooks';
+ 
+ 
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   Image,
+   TouchableOpacity,
+   Platform,
+   Alert,
+   Dimensions,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ import {useState} from 'react';
+ import WelcomeScreen from './screens/WelcomeScreen';
+ 
+ export const App: () => Node = () => {
+ 
+ 
+   return (
+       <WelcomeScreen />
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     // alignItems: 'center',
+     // justifyContent: 'center',
+     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+   },
+ });
+ 
+ export default App;
